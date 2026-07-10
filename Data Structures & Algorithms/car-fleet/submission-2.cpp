2b5cf533ebdb81d6fe72f51107b950db24366d71
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-07-10 18:22:37 +0700

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+
+        vector<pair<int, int>> cars(n);
+        vector<float> times;
+
+        for(int i = 0; i < n; i++) {
+            cars[i] = {position[i], speed[i]};
+        }
+
+        sort(cars.begin(), cars.end(), greater<pair<int,int>>());
+
+        
+        for(int i = 0; i < n; i++) {
+            float time = (float)(target-cars[i].first) / cars[i].second;
+
+            if(times.empty()) {
+                times.push_back(time);
+                continue;
+            }
+
+            float previous_time = times.back();
+
+            if(time > previous_time) {
+                times.push_back(time);
+            }
+        }
+
+
+
+        return times.size();
+    }
+};
