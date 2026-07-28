a6abc61e069ae42d99f976622537076cc3ad85db
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-07-28 17:14:39 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+
+        while(l <= r) {
+            int m = (l+r) / 2;
+
+            if(nums[m] == target) return m;
+            if(nums[m] < target) l += 1;
+            if(nums[m] > target) r -= 1;
+        }
+
+        return -1;
+    }
+};
